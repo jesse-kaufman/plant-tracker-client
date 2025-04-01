fc423d5dcda0afa43b8303b5d3d97fe3f2cf6d5f
jesse-kaufman <jesse@jessekaufman.com>
2025-04-01 11:14:21 -0500

    chore: init project

---
+++ vitest.config.js
@@ -0,0 +1,17 @@
+/** @file Vitest configuration. */
+import path from "path"
+import { defineConfig } from "vitest/config"
+import vue from "@vitejs/plugin-vue"
+
+export default defineConfig({
+  test: {
+    environment: "jsdom",
+  },
+
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "src"), // Ensure this resolves to /src
+    },
+  },
+  plugins: [vue()],
+})
