05c542dc968bd8b5c3b7fc26c0eb5b00284a0f48
jesse-kaufman <jesse@jessekaufman.com>
2025-04-01 18:05:23 -0500

    test: add placeholder for plant service unit tests

---
+++ __tests__/unit/plantServices.test.js
@@ -0,0 +1,8 @@
+/** @file Plant service unit tests. */
+import { describe, expect, it } from "vitest"
+
+describe("Date service", () => {
+  it("should return true when true", () => {
+    expect(true).toBe(true)
+  })
+})
