6d17fdc6367d3258208c7d3b265f3f21d3376907
jesse-kaufman <jesse@jessekaufman.com>
2025-04-01 19:56:25 -0500

    chore: upgrade to tailwindcss@4

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,11 @@ import { defineConfig } from "vite"
 import vue from "@vitejs/plugin-vue"
 import vueDevTools from "vite-plugin-vue-devtools"
 import svgLoader from "vite-svg-loader"
+import tailwindcss from "@tailwindcss/vite"
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueDevTools(), svgLoader()],
+  plugins: [vue(), vueDevTools(), svgLoader(), tailwindcss()],
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
