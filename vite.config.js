b7a1b7bdbcd1ca5e06f8cdfa3e997627fe1660c3
jesse-kaufman <jesse@jessekaufman.com>
2025-04-02 16:18:46 -0500

    chore: clean up vite config

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,9 @@ import { fileURLToPath, URL } from "node:url"
 import { defineConfig } from "vite"
 import vue from "@vitejs/plugin-vue"
 import vueDevTools from "vite-plugin-vue-devtools"
-import svgLoader from "vite-svg-loader"
 import tailwindcss from "@tailwindcss/vite"
+import svgLoader from "vite-svg-loader"
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [vue(), vueDevTools(), svgLoader(), tailwindcss()],
   resolve: {
