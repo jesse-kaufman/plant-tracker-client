8d58354471278ee1695af2455c1772321447c7ae
jesse-kaufman <jesse@jessekaufman.com>
2025-04-02 16:26:08 -0500

    chore: ignore eslint import/no-unresolved

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import { fileURLToPath, URL } from "node:url"
 import { defineConfig } from "vite"
 import vue from "@vitejs/plugin-vue"
 import vueDevTools from "vite-plugin-vue-devtools"
+// eslint-disable-next-line import/no-unresolved
 import tailwindcss from "@tailwindcss/vite"
 import svgLoader from "vite-svg-loader"
 
