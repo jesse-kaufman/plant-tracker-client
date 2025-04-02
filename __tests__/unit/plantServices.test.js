cba5fdaa42e7b8db71c7cb65bb8e58b81b71062a
jesse-kaufman <jesse@jessekaufman.com>
2025-04-01 19:00:57 -0500

    test: flesh out plant services unit tests

--- __tests__/unit/plantServices.test.js
+++ __tests__/unit/plantServices.test.js
@@ -1,18 +1,22 @@
+/* eslint-disable max-lines-per-function */
 /** @file Plant service unit tests. */
 import { describe, expect, it } from "vitest"
-import { getStageStartDate } from "@/services/plantServices.js"
+import {
+  getStageStartDate,
+  getStageDuration,
+} from "@/services/plantServices.js"
+
+const dates = {
+  startedOn: new Date("2023-01-01"),
+  vegStartedOn: new Date("2023-01-05"),
+  flowerStartedOn: new Date("2023-02-01"),
+  harvestedOn: new Date("2023-03-01"),
+  cureStartedOn: new Date("2023-03-05"),
+}
 
 describe("Plant service", () => {
   describe("getStageStartDate", () => {
     it("should get the stage start date correctly", () => {
-      const dates = {
-        startedOn: new Date("2023-01-01"),
-        vegStartedOn: new Date("2023-01-05"),
-        flowerStartedOn: new Date("2023-02-01"),
-        harvestedOn: new Date("2023-03-01"),
-        cureStartedOn: new Date("2023-03-05"),
-      }
-
       // Test getting startedOn date.
       expect(getStageStartDate("seedling", dates)).toEqual(dates.startedOn)
 
@@ -27,6 +31,47 @@ describe("Plant service", () => {
 
       // Test getting cureStartedOn date.
       expect(getStageStartDate("cure", dates)).toEqual(dates.cureStartedOn)
+    })
+
+    // Test sending invalid object for dates.
+    it("throws error if dates object is missing the expected property", () => {
+      expect(() => {
+        getStageStartDate("veg", {})
+      }).toThrow()
+    })
+
+    // Test sending non-object for dates.
+    it("throws error if dates object non-object", () => {
+      expect(() => {
+        getStageStartDate("veg", null)
+      }).toThrow()
+    })
+  })
+
+  describe("getStageDuration", () => {
+    it("should build the stage duration string properly", () => {
+      // Test a short duration.
+      expect(
+        getStageDuration(
+          "seedling",
+          new Date("2023-01-01"),
+          new Date("2023-01-05")
+        )
+      ).toBe("Week 1 (4 days) in seedling stage.")
+
+      // Test a longer duration in veg.
+      expect(
+        getStageDuration("veg", new Date("2023-01-01"), new Date("2023-03-12"))
+      ).toBe("Week 11 (70 days) in veg stage.")
+
+      // Test a longer duration.
+      expect(
+        getStageDuration(
+          "harvested",
+          new Date("2023-01-01"),
+          new Date("2023-02-01")
+        )
+      ).toBe("Week 5 (31 days) since harvest.")
     })
   })
 })
