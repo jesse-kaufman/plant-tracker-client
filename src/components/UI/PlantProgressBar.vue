ad6655b22cf577275e0da27faf1d92a8fecf7a64
jesse-kaufman <jesse@jessekaufman.com>
2025-04-03 11:44:45 -0500

    refactor: rename stage prop to currentStage

--- src/components/UI/PlantProgressBar.vue
+++ src/components/UI/PlantProgressBar.vue
@@ -2,23 +2,39 @@
   <ol class="flex items-center mb-4 w-full lg:mt-4 sm:ml-2 sm:pr-2 sm:mb-5">
     <PlantProgressBarItem
       id="source-seed"
-      :stage="stage"
+      :current-stage="currentStage"
       tooltip="Sourced from seed"
     />
 
     <PlantProgressBarItem
       id="seedling"
-      :current-stage="stage"
+      :current-stage="currentStage"
       tooltip="Seedling stage"
     />
 
-    <PlantProgressBarItem id="veg" :stage="stage" tooltip="Vegetative stage" />
+    <PlantProgressBarItem
+      id="veg"
+      :current-stage="currentStage"
+      tooltip="Vegetative stage"
+    />
 
-    <PlantProgressBarItem id="flower" :stage="stage" tooltip="Flower stage" />
+    <PlantProgressBarItem
+      id="flower"
+      :current-stage="currentStage"
+      tooltip="Flower stage"
+    />
 
-    <PlantProgressBarItem id="harvested" :stage="stage" tooltip="Harvested" />
+    <PlantProgressBarItem
+      id="harvested"
+      :current-stage="currentStage"
+      tooltip="Harvested"
+    />
 
-    <PlantProgressBarItem id="cure" :stage="stage" tooltip="In storage" />
+    <PlantProgressBarItem
+      id="cure"
+      :current-stage="currentStage"
+      tooltip="In storage"
+    />
   </ol>
 </template>
 
@@ -26,7 +42,7 @@
 import PlantProgressBarItem from "./PlantProgressBarItem.vue"
 
 defineProps({
-  stage: {
+  currentStage: {
     type: String,
     required: true,
   },
