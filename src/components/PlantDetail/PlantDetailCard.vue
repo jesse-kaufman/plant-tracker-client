479af0d9045e42b8da10eda150c6a1f9a565f2a1
jesse-kaufman <jesse@jessekaufman.com>
2025-04-03 13:21:29 -0500

    refactor: move loading plant data into view component

--- src/components/PlantDetail/PlantDetailCard.vue
+++ src/components/PlantDetail/PlantDetailCard.vue
@@ -24,26 +24,17 @@ import PlantCardHeader from "@/components/UI/PlantCardHeader.vue"
 import PlantProgressBar from "@/components/UI/PlantProgressBar.vue"
 import { getStageStartDate, getStageDates } from "@/services/plantServices.js"
 
-const plant = reactive({
-  id: 1,
-  name: "Heirloom tomatoes #1",
-  stage: "veg",
-  startedOn: new Date("2023-01-01T00:00:00"),
-  vegStartedOn: new Date("2023-01-05T00:00:00"),
-  flowerStartedOn: new Date("2023-01-28T00:00:00"),
-  harvestedOn: new Date("2023-03-31T00:00:00"),
-  cureStartedOn: new Date("2023-04-04T00:00:00"),
-  notes:
-    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+const props = defineProps({
+  plant: {
+    type: Object,
+    required: true,
+  },
 })
 
 // Extract stage dates from plant and keep them reactive.
-const dates = reactive(getStageDates(toRefs(plant)))
+const dates = reactive(getStageDates(toRefs(props.plant)))
 // Extract current stage start date as computed property.
-const stageStartedOn = computed(() => getStageStartDate(plant.stage, dates))
-
-// Expose the reactive data to the global window object after the component is mounted
-onMounted(() => {
-  window.vueApp = { plant }
-})
+const stageStartedOn = computed(() =>
+  getStageStartDate(props.plant.stage, dates)
+)
 </script>
--- src/views/PlantDetailView.vue
+++ src/views/PlantDetailView.vue
@@ -1,13 +1,33 @@
 <template>
   <div class="py-6 mb-10 sm:px-6 lg:px-8">
     <div class="flex flex-col p-4 lg:flex-row">
-      <PlantDetailCard />
+      <PlantDetailCard :plant="plant" />
       <PlantTimeline />
     </div>
   </div>
 </template>
 
 <script setup>
+import { onMounted, reactive } from "vue"
 import PlantDetailCard from "@/components/PlantDetail/PlantDetailCard.vue"
 import PlantTimeline from "@/components/PlantDetail/PlantTimeline.vue"
+
+import plantData from "@/plant.json"
+
+console.log(plantData)
+
+const plant = reactive({
+  ...plantData,
+  startedOn: plantData.startedOn && new Date(plantData.startedOn),
+  vegStartedOn: plantData.vegStartedOn && new Date(plantData.vegStartedOn),
+  flowerStartedOn:
+    plantData.flowerStartedOn && new Date(plantData.flowerStartedOn),
+  harvestedOn: plantData.harvestedOn && new Date(plantData.harvestedOn),
+  cureStartedOn: plantData.cureStartedOn && new Date(plantData.cureStartedOn),
+})
+
+// Expose the reactive data to the global window object after the component is mounted
+onMounted(() => {
+  window.vueApp = { plant }
+})
 </script>
