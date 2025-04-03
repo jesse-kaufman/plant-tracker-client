aad231ef9f60ab3f48aa7989c6e59cef9761061c
jesse-kaufman <jesse@jessekaufman.com>
2025-04-03 08:40:46 -0500

    feat: add BaseIcon and BaseTooltip components

---
+++ src/components/UI/BaseIcon.vue
@@ -0,0 +1,28 @@
+<template>
+  <bellIcon v-if="icon === 'bell'" />
+  <flowerIcon v-if="icon === 'flower'" />
+  <harvestedIcon v-if="icon === 'harvested'" />
+  <homeIcon v-if="icon === 'home'" />
+  <seedlingIcon v-if="icon === 'seedling'" />
+  <sourceSeedIcon v-if="icon === 'source-seed'" />
+  <storageIcon v-if="icon === 'cure' || icon === 'storage'" />
+  <vegIcon v-if="icon === 'veg'" />
+</template>
+
+<script setup>
+import bellIcon from "@/assets/icons/bell.svg"
+import flowerIcon from "@/assets/icons/flower.svg"
+import harvestedIcon from "@/assets/icons/harvested.svg"
+import homeIcon from "@/assets/icons/home.svg"
+import seedlingIcon from "@/assets/icons/seedling.svg"
+import sourceSeedIcon from "@/assets/icons/source-seed.svg"
+import storageIcon from "@/assets/icons/storage.svg"
+import vegIcon from "@/assets/icons/veg.svg"
+
+defineProps({
+  icon: {
+    type: String,
+    required: true,
+  },
+})
+</script>
---
+++ src/components/UI/BaseTooltip.vue
@@ -0,0 +1,18 @@
+<template>
+  <div
+    :id="`tooltip-${id}`"
+    role="tooltip"
+    class="inline-block absolute invisible z-10 px-3 py-2 text-sm font-medium text-white bg-gray-900 rounded-lg opacity-0 transition-opacity duration-300 shadow-xs tooltip dark:bg-gray-700"
+  >
+    <slot />
+  </div>
+</template>
+
+<script setup>
+defineProps({
+  id: {
+    type: String,
+    required: true,
+  },
+})
+</script>
