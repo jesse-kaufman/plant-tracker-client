7a865eaff2b6f4a1a15119fda2f33134843ff862
jesse-kaufman <jesse@jessekaufman.com>
2025-04-03 11:44:13 -0500

    fix: add cureStartedOn date to plant object

--- src/components/PlantDetail/PlantDetailCard.vue
+++ src/components/PlantDetail/PlantDetailCard.vue
@@ -32,6 +32,7 @@ const plant = reactive({
   vegStartedOn: new Date("2023-01-05T00:00:00"),
   flowerStartedOn: new Date("2023-01-28T00:00:00"),
   harvestedOn: new Date("2023-03-31T00:00:00"),
+  cureStartedOn: new Date("2023-04-04T00:00:00"),
   notes:
     "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
 })
