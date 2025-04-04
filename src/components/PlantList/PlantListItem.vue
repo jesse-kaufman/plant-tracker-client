a69d61f906518522b1e54daa837d02f2bf4355bd
jesse-kaufman <jesse@jessekaufman.com>
2025-04-03 19:07:45 -0500

    feat: add plant list components

---
+++ src/components/PlantList/PlantListItem.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="p-4 w-1/1 md:w-1/2 lg:w-1/3">
+    <div :class="['plant-card', stage]">
+      <PlantCardHeader
+        :stage="stage"
+        :name="currentPlant.name"
+        :stage-started-on="stageStartedOn"
+      />
+
+      <div class="flex flex-row mt-3 ml-2">
+        <p class="text-sm leading-tight">
+          <strong v-if="plant.status !== 'archived'">Stage started on:</strong>
+          <strong v-if="plant.status === 'archived'">Archived on:</strong>
+          <br />
+          {{
+            stageStartedOn.toLocaleDateString("en-US", {
+              year: "numeric",
+              month: "long",
+              day: "numeric",
+            })
+          }}
+        </p>
+
+        <BaseButton href="#">View</BaseButton>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import PlantCardHeader from "@/components/UI/PlantCardHeader.vue"
+import BaseButton from "@/components/UI/BaseButton.vue"
+import {
+  getStageStartDate,
+  getStageDates,
+  convertStageDates,
+} from "@/services/plantServices.js"
+
+const props = defineProps({
+  plant: {
+    type: Object,
+    required: true,
+  },
+  archived: {
+    type: Boolean,
+    default: false,
+  },
+})
+
+const stage = props.plant.status === "archived" ? "archived" : props.plant.stage
+
+// Convert date strings to date objects.
+const currentPlant = convertStageDates(props.plant)
+// Extract stage dates from plant.
+const dates = getStageDates(currentPlant)
+// Extract current stage start date.
+const stageStartedOn = getStageStartDate(props.plant.stage, dates)
+</script>
---
+++ src/views/PlantListView.vue
@@ -0,0 +1,33 @@
+<template>
+  <PlantListSection title="Active plants">
+    <PlantListItem
+      v-for="plant in activePlants"
+      :key="plant.id"
+      :plant="plant"
+    />
+  </PlantListSection>
+
+  <PlantListSection title="Archived plants">
+    <PlantListItem
+      v-for="plant in archivedPlants"
+      :key="plant.id"
+      :plant="plant"
+      :archived="true"
+    />
+  </PlantListSection>
+</template>
+
+<script setup>
+import { computed, reactive } from "vue"
+import PlantListSection from "@/components/PlantList/PlantListSection.vue"
+import PlantListItem from "@/components/PlantList/PlantListItem.vue"
+import plantData from "@/plants.json"
+
+const plants = reactive(plantData)
+const activePlants = computed(() =>
+  plants.filter((plant) => plant.status === "active")
+)
+const archivedPlants = computed(() =>
+  plants.filter((plant) => plant.status === "archived")
+)
+</script>
