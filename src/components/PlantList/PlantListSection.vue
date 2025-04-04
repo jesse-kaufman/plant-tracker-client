c69703aa40fdeab6650ac858dee48387eb9e70f3
jesse-kaufman <jesse@jessekaufman.com>
2025-04-03 19:06:03 -0500

    feat: add header and define props for header

--- src/components/PlantList/PlantListSection.vue
+++ src/components/PlantList/PlantListSection.vue
@@ -1,7 +1,20 @@
 <template>
+  <header v-if="$slots.default" class="plant-list-section-header">
+    <h1 class="text-2xl">{{ title }}</h1>
+  </header>
+
   <div class="py-6 mb-10 sm:px-6 lg:px-8">
     <div class="plant-list">
       <slot />
     </div>
   </div>
 </template>
+
+<script setup>
+defineProps({
+  title: {
+    type: String,
+    required: true,
+  },
+})
+</script>
