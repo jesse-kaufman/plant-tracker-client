b50ed945954aaa1a82120fce880ec4397d2212e4
jesse-kaufman <jesse@jessekaufman.com>
2025-04-03 19:07:25 -0500

    refactor: use semantic CSS classes for brevity

--- src/components/UI/PlantCardHeader.vue
+++ src/components/UI/PlantCardHeader.vue
@@ -1,17 +1,11 @@
 <template>
-  <div class="flex items-center mb-7">
+  <div class="plant-card-header">
     <div class="plant-stage-icon">
       <BaseIcon :icon="stage" />
     </div>
     <div>
-      <h2
-        class="font-bold text-4xl/8 sm:text-5xl/9 text-white/40 font-stretch-condensed"
-      >
-        {{ name }}
-      </h2>
-      <div
-        class="mt-1 italic font-semibold leading-tight sm:mt-2 text-md sm:text-lg text-white/70"
-      >
+      <h2>{{ name }}</h2>
+      <div class="stage-duration">
         {{ stageDuration }}
       </div>
     </div>
