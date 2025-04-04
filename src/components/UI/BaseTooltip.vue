e860c56c6802f4e001f433402251045de0c827a7
jesse-kaufman <jesse@jessekaufman.com>
2025-04-03 19:06:30 -0500

    chore: clean up CSS classes

--- src/components/UI/BaseTooltip.vue
+++ src/components/UI/BaseTooltip.vue
@@ -2,7 +2,7 @@
   <div
     :id="`tooltip-${id}`"
     role="tooltip"
-    class="inline-block absolute invisible z-10 px-3 py-2 text-sm font-medium text-white bg-gray-900 rounded-lg opacity-0 transition-opacity duration-300 shadow-xs tooltip dark:bg-gray-700"
+    class="inline-block absolute invisible z-10 px-3 py-2 text-sm font-medium text-white bg-gray-700 rounded-lg opacity-0 transition-opacity duration-300 shadow-xs"
   >
     <slot />
   </div>
