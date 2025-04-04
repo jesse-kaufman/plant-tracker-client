d3c10581e7a8e653650abca4d459e5a3dffc1ee5
jesse-kaufman <jesse@jessekaufman.com>
2025-04-03 20:20:25 -0500

    feat: add router configuration

---
+++ src/router/index.js
@@ -0,0 +1,29 @@
+/** @file Vue routes. */
+import { createRouter, createWebHistory } from "vue-router"
+
+const routes = [
+  // Redirect '/' to '/plants'.
+  {
+    path: "/",
+    redirect: "/plants",
+  },
+  // View plants.
+  {
+    path: "/plants",
+    component: () => import("@/views/PlantListView.vue"),
+  },
+  // View plant.
+  {
+    path: "/plants/:id",
+    component: () => import("@/views/PlantDetailView.vue"),
+  },
+  // 404 page.
+  { path: "/:pathMatch(.*)*", component: import("@/views/LostView.vue") },
+]
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+})
+
+export default router
