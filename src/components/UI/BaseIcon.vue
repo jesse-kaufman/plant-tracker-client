f04414074eb1539284e828c8b4d597b58908e913
jesse-kaufman <jesse@jessekaufman.com>
2025-04-03 19:06:11 -0500

    feat: add archived icon

--- src/components/UI/BaseIcon.vue
+++ src/components/UI/BaseIcon.vue
@@ -7,6 +7,7 @@
   <sourceSeedIcon v-if="icon === 'source-seed'" />
   <storageIcon v-if="icon === 'cure' || icon === 'storage'" />
   <vegIcon v-if="icon === 'veg'" />
+  <archivedIcon v-if="icon === 'archived'" />
 </template>
 
 <script setup>
@@ -18,6 +19,7 @@ import seedlingIcon from "@/assets/icons/seedling.svg"
 import sourceSeedIcon from "@/assets/icons/source-seed.svg"
 import storageIcon from "@/assets/icons/storage.svg"
 import vegIcon from "@/assets/icons/veg.svg"
+import archivedIcon from "@/assets/icons/archived.svg"
 
 defineProps({
   icon: {
