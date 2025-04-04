aa8aadf03fc869ee3033942255c7d7567c455b37
jesse-kaufman <jesse@jessekaufman.com>
2025-04-03 20:16:37 -0500

    feat: add catch all 404 view

---
+++ src/views/LostView.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="flex flex-col justify-center items-center h-screen">
+    <h1 class="text-8xl">4 8 15 16 23 42</h1>
+    <p class="mt-2 text-4xl text-center">You appear to be lost.</p>
+  </div>
+</template>
