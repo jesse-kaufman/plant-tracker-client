a546a319ad327698cab6a60f0c88e719c7861603
jesse-kaufman <jesse@jessekaufman.com>
2025-04-07 19:47:34 -0500

    feat: add fetchPlants()

--- src/services/plantService.js
+++ src/services/plantService.js
@@ -3,6 +3,27 @@ import plants from "@/plants.json"
 import { convertStageDates } from "@/utils/plantUtils"
 
 /**
+ * Fetch plants from API.
+ * @param {string} status - Status of plants to retrieve; defaults to active.
+ * @returns {object} Plant object.
+ */
+export const fetchPlants = async (status = "active") => {
+  try {
+    console.log(`fetching ${status} plants: `)
+    const response = await fetch(`/api/plants`)
+
+    if (!response.ok) {
+      throw new Error("Plant not found")
+    }
+
+    return await response.json()
+  } catch (error) {
+    console.error("Error fetching plant:", error)
+    throw error
+  }
+}
+
+/**
  * Fetches plant from API.
  * @param {string} id - ID of plant to fetch.
  * @returns {object} Plant object.
